629fee1e391a1ab9ab83f52b6223e8f79c84422e
Malek Ascha <malekascha@gmail.com>
2016-05-12 11:48:27 -0700

    removed old code

--- server/crime/crimeRoutes.js
+++ server/crime/crimeRoutes.js
@@ -3,5 +3,4 @@ var crimeController = require('./crimeController.js');
 module.exports = function(app){
   app.get('/getAllCrimes', crimeController.getAllCrimes);
   app.post('/addNewCrime', crimeController.addNewCrime);
-  app.get('/video', crimeController.getVideo);
 }
